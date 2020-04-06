7c1b69cc49492f36d689878660da3f7b6fdd5743
venkatesh152 <63138853+venkatesh152@users.noreply.github.com>
2020-04-06 12:02:30 +0530

    c-three

--- project13/app13/views.py
+++ project13/app13/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 def showindex(req):
      s={
         "101":{"name":"venkat","marks":[20,30,40,50,60,70]},
-        "102":{"name":"sraven","marks":[55,25,35,45,55,85]},
+        "102":{"name":"sraven","marks":[55,25,35,45,55,100]},
         "103":{"name":"jagadesh","marks":[21, 31, 41, 51, 61, 71]},
         "104": {"name": "MANOHAR","marks":[10,35,26,63,30,35]}
     }
